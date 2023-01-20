2468d8c7b8d8d719acf45572535fadf345758cdb
Liguang Zhang <liguangzhang@Liguangs-MacBook-Pro.local>
2023-01-19 23:41:37 -0500

    edited readme and final tweaks to js file

--- Develop/script.js
+++ Develop/script.js
@@ -2,67 +2,65 @@
 let generateBtn = document.querySelector("#generate");
 
 // user click, leads to 5 prompt for password criteria, entries stored
-
 generateBtn.addEventListener("click", start_generator);
 
 let user_input = [false, false, false, false];
 let proceed =  user_input[0] || user_input[1] || user_input[2] || user_input[3];
 let password_length;
+let password="";
   //the characters to generate the password from
 let available = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ","0123456789", "!@#$%^&*();:"];
 let chars="";
 console.log(user_input);
 
 function start_generator(){
-  while (proceed==false) {
-    criteria_gathering();
+  criteria_gathering();
+}
 
-    function criteria_gathering(){
-      password_length = window.prompt("What is your desired password length?", "Enter a number between 8 to 128");
+function criteria_gathering(){
+    password_length = window.prompt("What is your desired password length?", "Enter a number between 8 to 128");
 
-      if((password_length<8)||(password_length>128)){
-        window.alert("Please enter valid password length!");
-        return;
-      }
-
-      user_input[0] = window.prompt("Do you wish to include lowercase characters?", "Y for yes or N for no").toUpperCase;
-      user_input[1] = window.prompt("Do you wish to include uppercase characters?", "Y for yes or N for no").toUpperCase;
-      user_input[2] = window.prompt("Do you wish to include numbers?", "Y for yes or N for no").toUpperCase;
-      user_input[3] = window.prompt("Do you wish to include special characters such as -'?,.?", "Y for yes or N for no").toUpperCase;
-
-      console.log(user_input);
-      for (i=0; i<user_input.length; i++){
-        if (user_input[i]== "Y") {
-          user_input[i]==true;
-          chars+= available[i];
-        } else if (user_input[i]== "N"){
-          user_input[i]==false;
-        } else { window.alert("Please make sure you enter only Y or N!");
-        return;
-        }
-      }
-      }
-      proceed = user_input[0] || user_input[1] || user_input[2] || user_input[3];
-      console.log(proceed);
-      if (proceed==false) {
-        window.alert("Please choose at least one character criteria!");
-      }
-    
+    if((password_length<8)||(password_length>128)){
+      window.alert("Please enter valid password length!");
+      return;
     }
-  }
-  console.log("proceed works");
-  generatePassword();
 
+    user_input[0] = window.prompt("Do you wish to include lowercase characters?", "Y for yes or N for no");
+    user_input[1] = window.prompt("Do you wish to include uppercase characters?", "Y for yes or N for no");
+    user_input[2] = window.prompt("Do you wish to include numbers?", "Y for yes or N for no");
+    user_input[3] = window.prompt("Do you wish to include special characters such as -'?,.?", "Y for yes or N for no");
 
+    console.log(user_input);
+    for (i=0; i<user_input.length; i++){
+      if (user_input[i]== 'Y') {
+        user_input[i]=true;
+        chars+= available[i];
+      } else if (user_input[i]== 'N'){
+        user_input[i]=false;
+      } else { window.alert("Please make sure you enter only Y or N!");
+      return;
+      }
+    }
 
 console.log(chars);
-// functino to generate password
-let password="";
+
+    if (!user_input[0] && !user_input[1] && !user_input[2] && !user_input[3]){
+      window.alert("Please choose at least one character criteria!");
+    }
+    generatePassword();
+
+  }
+
+
+
+
+// function to generate password
+
 function generatePassword(){
  
   //random letter/number generator to generate each letter/number of the password
   for(i=0; i<password_length; i++){
-    let random_index = Math.floor(Math.random())*chars.length;
+    let random_index = Math.floor(Math.random()*chars.length);
     password += chars.substring(random_index, random_index+1);
     console.log(password);
   }
@@ -76,5 +74,4 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Password_generator
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+Eployees who may have access to access to sensitive data would need to have a strong password to provide security to that information. This password generator gives its user choices to compose their own password using five criteria: password-length, upper-case and lower-case letters, numbers and special characters. 
+
+
+## Installation
+
+Use any web browser to access the webpage. If you wish to see the source code, please the clone the github repo to your computer and edit using any IDE for html, css and javascript. 
+
+## Usage
+
+To access the webpage, go to https://liguang-lee-zhang.github.io/Module-1-Challenge/. 
+
+Image showing deployed webpage:
+
+
+![alt Deployed webpage](/assets/images/screenshot.png)
+
+
+
+## License
+
+This project is under a MIT license. 
+
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
