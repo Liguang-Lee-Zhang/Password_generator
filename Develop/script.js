ab0ebc57e72a5e31d8bf96589c6688f8e68e0c01
Liguang Zhang <liguangzhang@Liguangs-MacBook-Pro.local>
2023-01-19 22:25:33 -0500

    updated js file

--- Develop/script.js
+++ Develop/script.js
@@ -8,31 +8,39 @@ generateBtn.addEventListener("click", start_generator);
 let user_input = [false, false, false, false];
 let proceed =  user_input[0] || user_input[1] || user_input[2] || user_input[3];
 let password_length;
-
+  //the characters to generate the password from
+let available = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ","0123456789", "!@#$%^&*();:"];
+let chars="";
 console.log(user_input);
+
 function start_generator(){
   while (proceed==false) {
     criteria_gathering();
-    function criteria_gathering(){
-      password_length = window.prompt("What is your desired password length?", "Less than 8 or between 8 to 128 characters");
 
-      if(((password_length=="between 8 to 128 characters")||(password_length=="Less than 8"))==false){
-        window.alert("Please enter valid password length! Entry is case-sensitive.");
+    function criteria_gathering(){
+      password_length = window.prompt("What is your desired password length?", "Enter a number between 8 to 128");
+
+      if((password_length<8)||(password_length>128)){
+        window.alert("Please enter valid password length!");
         return;
       }
 
-      user_input[0] = window.prompt("Do you wish to include lowercase characters?", "Y for yes or N for no");
-      user_input[1] = window.prompt("Do you wish to include uppercase characters?", "Y for yes or N for no");
-      user_input[2] = window.prompt("Do you wish to include numbers?", "Y for yes or N for no");
-      user_input[3] = window.prompt("Do you wish to include special characters such as -'?,.?", "Y for yes or N for no");
+      user_input[0] = window.prompt("Do you wish to include lowercase characters?", "Y for yes or N for no").toUpperCase;
+      user_input[1] = window.prompt("Do you wish to include uppercase characters?", "Y for yes or N for no").toUpperCase;
+      user_input[2] = window.prompt("Do you wish to include numbers?", "Y for yes or N for no").toUpperCase;
+      user_input[3] = window.prompt("Do you wish to include special characters such as -'?,.?", "Y for yes or N for no").toUpperCase;
 
       console.log(user_input);
       for (i=0; i<user_input.length; i++){
         if (user_input[i]== "Y") {
           user_input[i]==true;
-        } else {
+          chars+= available[i];
+        } else if (user_input[i]== "N"){
           user_input[i]==false;
+        } else { window.alert("Please make sure you enter only Y or N!");
+        return;
         }
+      }
       }
       proceed = user_input[0] || user_input[1] || user_input[2] || user_input[3];
       console.log(proceed);
@@ -45,44 +53,21 @@ function start_generator(){
   console.log("proceed works");
   generatePassword();
 
-}
 
 
-
-/*
-if (proceed==Y) {
-  console.log("proceed works");
-  generatePassword();
- 
-}
-
-*/
-
+console.log(chars);
 // functino to generate password
 let password="";
 function generatePassword(){
-
-  //the characters to generate the password from
-  let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let password_length=0;
-  //random number generator to generate a number between 8 to 128 
-  while (password_length<8){
-    password_length = Math.floor(Math.random())* 128;
-  }
-  
-
+ 
   //random letter/number generator to generate each letter/number of the password
-  for(i=0; i<password_length.length; i++){
+  for(i=0; i<password_length; i++){
     let random_index = Math.floor(Math.random())*chars.length;
     password += chars.substring(random_index, random_index+1);
     console.log(password);
   }
-  console.log(password);
+  console.log(password.length);
   writePassword();
-
-  //while loop to check that the password satisfies user selected criteria
-
-
 }
 
 
